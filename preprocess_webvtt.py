d111e6892cd6df816831bb949d05fe6ed99f4f47
jano_desktop <JanHusarcik@users.noreply.github.com>
2025-11-04 13:51:19 +0100

    Added:
    - support for file _and_ folder processing.
    - try, except in `process_vtt()`
    - type declaration

--- preprocess_webvtt.py
+++ preprocess_webvtt.py
@@ -3,63 +3,77 @@ import re
 import glob
 import os
 import argparse
+from typing import List
 
 
-def process_vtt(file):
+def process_vtt(file:str):
     all_caps: bool = True
     newline_in_previous: bool = False
-    with open(f"{file}.vtt", "w", encoding="utf-8") as f:
-        for caption in webvtt.read(file):
-            if re.search(r"[a-z]", caption.text):
-                all_caps = False
-            fragment: str = ""
-            fragment += f"⎡⎡{caption.start} --> {caption.end}⎦⎦ "
-            # multiple speakers
-            if caption.raw_text.startswith("-"):
-                fragment += (
-                    "\n".join(
-                        re.sub(r"^-(\s*[A-Z]+:)?", r"⎡⎡Speaker \1⎦⎦ ", line)
-                        for line in caption.lines
+    try:
+        with open(f"{file}.vtt", "w", encoding="utf-8") as f:
+            for caption in webvtt.read(file):
+                if re.search(r"[a-z]", caption.text):
+                    all_caps = False
+                fragment: str = ""
+                fragment += f"⎡⎡{caption.start} --> {caption.end}⎦⎦ "
+                # multiple speakers
+                if caption.raw_text.startswith("-"):
+                    fragment += (
+                        "\n".join(
+                            re.sub(r"^-(\s*[A-Z]+:)?", r"⎡⎡Speaker \1⎦⎦ ", line)
+                            for line in caption.lines
+                        )
+                        + " "
                     )
-                    + " "
-                )
-            else:
-                cue_text = " ".join(caption.raw_text.splitlines()) + " "
-                fragment += re.sub(r"^([A-Z]+:)", r"⎡⎡Speaker \1⎦⎦ ", cue_text)
-            # sounds in brackets
-            if re.match(r"^\[[^\]]*\]$", caption.raw_text) or re.match(
-                r"⎡⎡Speaker[^⎦]*?⎦⎦ *\[[^\]]*\]", caption.raw_text
-            ):
-                if newline_in_previous:
-                    fragment += "\n"
                 else:
-                    fragment = "\n" + fragment + "\n"
-                newline_in_previous = True
-            elif fragment.endswith("] "):
-                fragment += "\n"
-                newline_in_previous = True
-            # break after punctuation
-            elif re.search(r"[!?\.]$", caption.text):
-                fragment += "\n"
-                newline_in_previous = True
-            else:
-                newline_in_previous = False
-            f.write(re.sub(" +", " ", fragment))
+                    cue_text = " ".join(caption.raw_text.splitlines()) + " "
+                    fragment += re.sub(r"^([A-Z]+:)", r"⎡⎡Speaker \1⎦⎦ ", cue_text)
+                # sounds in brackets
+                if re.match(r"^\[[^\]]*\]$", caption.raw_text) or re.match(
+                    r"⎡⎡Speaker[^⎦]*?⎦⎦ *\[[^\]]*\]", caption.raw_text
+                ):
+                    if newline_in_previous:
+                        fragment += "\n"
+                    else:
+                        fragment = "\n" + fragment + "\n"
+                    newline_in_previous = True
+                elif fragment.endswith("] "):
+                    fragment += "\n"
+                    newline_in_previous = True
+                # break after punctuation
+                elif re.search(r"[!?\.]$", caption.text):
+                    fragment += "\n"
+                    newline_in_previous = True
+                else:
+                    newline_in_previous = False
+                f.write(re.sub(" +", " ", fragment))
+    except Exception as e:
+        print(f"Error processing file {file}: {e}")
+        return
 
     if all_caps:
         print("All captions are in uppercase.")
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Process all .webvtt files in a folder."
     )
-    parser.add_argument("folder", help="Path to the folder containing .webvtt files")
+    parser.add_argument(
+        "path", help="Path to the file, or folder containing .webvtt files")
     args = parser.parse_args()
-
-    folder = args.folder
-    pattern = os.path.join(folder, "**", "*.webvtt")
-    for vtt_file in glob.glob(pattern, recursive=True):
+    path = args.path
+    files: List[str] = []
+    if os.path.isfile(path):
+        files.append(path)
+    elif os.path.isdir(path):
+        pattern = os.path.join(path, "**", "*.webvtt")
+        files.extend(glob.glob(pattern, recursive=True))
+    else:
+        raise Exception(f"Path {path} is not valid.")
+    for vtt_file in files:
         process_vtt(vtt_file)
+
 
 if __name__ == "__main__":
     main()
