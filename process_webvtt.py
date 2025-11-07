bc5a783a225d749f0dfd0c0a7421c5227df7e4c1
jano_ntb <JanHusarcik@users.noreply.github.com>
2025-11-07 14:16:10 +0100

    added concurrency and progress bar

--- process_webvtt.py
+++ process_webvtt.py
@@ -5,6 +5,16 @@ import os
 import helpers.logging
 import helpers.postprocess
 import helpers.preprocess
+import threading
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import alive_progress
+
+MAX_CONCURRENT = 4  # Adjust as needed
+
+
+def process_with_semaphore(func, vtt_file, log, semaphore):
+    with semaphore:
+        func(vtt_file, log)
 
 
 def main():
@@ -28,12 +38,41 @@ def main():
     else:
         log.exception("Invalid path", path=path)
         raise Exception(f"Path {path} is not valid.")
+
+    semaphore = threading.Semaphore(MAX_CONCURRENT)
+    executor = ThreadPoolExecutor(max_workers=MAX_CONCURRENT)
+    futures = []
+
     if args.action == "prepare":
         for vtt_file in files:
-            helpers.preprocess.process_vtt(vtt_file, log)
+            futures.append(
+                executor.submit(
+                    process_with_semaphore,
+                    helpers.preprocess.process_vtt,
+                    vtt_file,
+                    log,
+                    semaphore,
+                )
+            )
     if args.action == "finalize":
         for vtt_file in files:
-            helpers.postprocess.process_vtt(vtt_file, log)
+            futures.append(
+                executor.submit(
+                    process_with_semaphore,
+                    helpers.postprocess.process_vtt,
+                    vtt_file,
+                    log,
+                    semaphore,
+                )
+            )
+    with alive_progress.alive_bar(
+        len(futures), title="Processing files", enrich_print=False
+    ) as bar:
+        for future in as_completed(futures):
+            # Will raise exceptions if any occurred in the worker threads
+            future.result()
+            bar()
+
     log.info("Done.")
 
 
